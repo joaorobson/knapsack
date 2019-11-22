ed45eba50792a40d9e9f3b92638889cb0f2ceab8
joaorobson <joaorobsonmartins@gmail.com>
2019-11-21 22:52:44 -0300

    Add form to add items to the knapsack

--- src/App.js
+++ src/App.js
@@ -1,26 +1,101 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import { Button, Grid, Form, List, Header, Image } from "semantic-ui-react";
+import "semantic-ui-css/semantic.min.css";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class Knapsack extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      items: [
+        { id: 1, mass: 42, value: 1 },
+        { id: 2, mass: 45, value: 3 }
+      ]
+    };
+  }
+
+  addItem() {
+    let items = this.state.items;
+    items.push({
+      id: this.state.items.length + 1,
+      mass: this.state.mass,
+      value: this.state.value
+    });
+
+    this.setState({ items: items });
+  }
+
+  removeItem(id) {
+    this.setState(prevState => ({
+      items: prevState.items.filter(el => el.id != id)
+    }));
+  }
+
+  itemsList() {
+    return (
+      <List celled ordered>
+        {this.state.items.map(item => (
+          <List.Item>
+            <Button
+              onClick={() => this.removeItem(item.id)}
+              color="red"
+              floated="right"
+            >
+              Remover
+            </Button>
+            <List.Content>
+              <List.Description>
+                <b>Massa:</b> {item.mass}
+              </List.Description>
+              <List.Description>
+                <b>Valor:</b> {item.value}
+              </List.Description>
+            </List.Content>
+          </List.Item>
+        ))}
+      </List>
+    );
+  }
+
+  handleChange = (e, { name, value }) => this.setState({ [name]: value });
+
+  render() {
+    return (
+      <Grid columns="equal" style={{ margin: "40px" }}>
+        <Header>Knapsack</Header>
+        <Grid.Row>
+          <Grid.Column>
+            <Form>
+              <Form.Group widths="equal">
+                <Form.Input
+                  name="mass"
+                  onChange={this.handleChange}
+                  type="number"
+                  min="0"
+                  fluid
+                  placeholder="Insira a massa do item"
+                />
+                <Form.Input
+                  name="value"
+                  onChange={this.handleChange}
+                  type="number"
+                  min="0"
+                  fluid
+                  placeholder="Insira o valor do item"
+                />
+              </Form.Group>
+              <Form.Button
+                onClick={this.addItem.bind(this)}
+                disabled={!this.state.value || !this.state.mass}
+              >
+                Adicionar item à mochila
+              </Form.Button>
+            </Form>
+          </Grid.Column>
+          <Grid.Column>{this.itemsList()}</Grid.Column>
+        </Grid.Row>
+      </Grid>
+    );
+  }
 }
 
-export default App;
+export default Knapsack;
